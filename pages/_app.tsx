edfd5f5fca920f8489c3e2d2d32280ea73f0b5ab
FE.hyunsu <fe.hyunsu@gmail.com>
2022-11-05 17:39:22 +0900

    add, update : global style set.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../styles/global-style';
+import { theme } from '../styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
---
+++ styles/global-style.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+export const GlobalStyle = createGlobalStyle`
+    ${reset}
+    :focus {
+        outline: none;
+        border: none;
+    }
+    ::-webkit-scrollbar {
+        display: none;
+    }
+    html{
+        font-size: 10px;
+        -webkit-text-size-adjust: none;
+        font-family: -apple-system,BlinkMacSystemFont,helvetica,Apple SD Gothic Neo,sans-serif;       
+        font-display: fallback;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+    button {
+        background: none;
+        padding: 0;
+        border: none;
+        cursor: pointer;
+    }
+`;
