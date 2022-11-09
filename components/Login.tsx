4165d1824074917cf0590d0fc308135371713271
FE.hyunsu <fe.hyunsu@gmail.com>
2022-11-09 23:30:53 +0900

    modify : firebase login set.

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseClientAuth } from '../client/firebase';
 
-interface CardProps {
-  title: string;
-  content: string;
-}
-
-const Login = ({ title, content }: CardProps) => {
+const Login = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const login = async () => {
+    await signInWithEmailAndPassword(firebaseClientAuth, email, password)
+      .then((userCredential) => {
+        // login 성공.
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode + ' / ' + errorMessage);
+      });
+  };
   return (
     <>
-      <h3>{title}</h3>
-      <p>{content}</p>
+      <div>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+        <button onClick={login}>로그인</button>
+      </div>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,14 @@
 import React, { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
 import Login from '../components/Login';
-import { firebaseClientAuth } from '../client/firebase';
 
 export default function Home() {
   const name: string = 'mason';
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
   const [jwt, setJwt] = useState<String>('');
-  const login = async () => {
-    // step1. Firebase 로그인
-    const credential = await signInWithEmailAndPassword(firebaseClientAuth, email, password);
-    console.log(signInWithEmailAndPassword);
-    console.log(firebaseClientAuth);
-    console.log(credential);
-
-    // 2. JWT 생성
-    const idToken = await credential.user.getIdToken();
-    // console.log(idToken);
-    setJwt(idToken);
-  };
 
   return (
     <>
       <div>hello, {name}</div>
-      <div>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
-        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
-        <button onClick={login}>로그인</button>
-      </div>
+      <Login />
       <div>
         로그인 결과 jwt 토큰 가져오기
         <br />
