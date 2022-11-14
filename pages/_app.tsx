b2356fc45e49c2d1411c6140718a74f42188476e
FE.hyunsu <fe.hyunsu@gmail.com>
2022-11-14 22:21:42 +0900

    add, modify : head set.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../styles/global-style';
 import { theme } from '../styles/theme';
@@ -6,6 +7,14 @@ import { theme } from '../styles/theme';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="AccountBook" />
+        <meta property="og:description" content="React study group accountBook" />
+        <meta property="og:image" content="/preview.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>🥸 AccountBook Admin</title>
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
