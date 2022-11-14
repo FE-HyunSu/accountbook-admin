6f9ddb73fac6ba4cc2a3e7a1dc4347ccd277ce1b
FE.hyunsu <fe.hyunsu@gmail.com>
2022-11-14 22:06:40 +0900

    modify : login update.

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,8 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { IntroBox, LoginBox } from './style';
+import { login } from '../../firebase/firestore';
 
-const Intro = () => {
+const Login = () => {
   const [renderCheck, setRenderCheck] = useState<boolean>(false);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  const loginGo = (email: HTMLInputElement | null, password: HTMLInputElement | null) => {
+    if (email !== null && password !== null) {
+      console.log(email.value);
+      console.log(password.value);
+      login(email.value, password.value);
+    }
+  };
+
   useEffect(() => {
     setRenderCheck(true);
   });
@@ -20,9 +31,11 @@ const Intro = () => {
               </strong>
             </dt>
             <dd>
-              <input type="text" placeholder="이메일을 입력해주세요." />
-              <input type="password" placeholder="비밀번호를 입력해주세요." />
-              <button type="button">로그인</button>
+              <input type="text" ref={emailRef} placeholder="이메일을 입력해주세요." />
+              <input type="password" ref={passwordRef} placeholder="비밀번호를 입력해주세요." />
+              <button type="button" onClick={() => loginGo(emailRef.current, passwordRef.current)}>
+                로그인
+              </button>
             </dd>
           </dl>
         </LoginBox>
@@ -31,4 +44,4 @@ const Intro = () => {
   );
 };
 
-export default Intro;
+export default Login;
