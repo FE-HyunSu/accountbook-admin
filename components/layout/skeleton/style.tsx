185bc16aa6076816b77c43809340a1d8ba65de2f
fe.hyunsu <fe.hyunsu@gmail.com>
2022-11-30 23:21:53 +0900

    modify: skeleton style.

--- components/layout/skeleton/style.tsx
+++ components/layout/skeleton/style.tsx
@@ -1,9 +1,32 @@
 import styled, { keyframes } from "styled-components";
 
+const effectBg = keyframes`
+  0%{left:-20%}
+  100%{left:120%}
+`;
+
 export const SkeletonUi = styled.span`
   display: inline-block;
+  position: relative;
   min-width: 5rem;
   min-height: 1rem;
   border-radius: 0.2rem;
   background-color: #eee;
+  overflow: hidden;
+  &:before {
+    content: "";
+    position: absolute;
+    top: -100%;
+    left: -20%;
+    width: 30%;
+    height: 300%;
+    background: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0.6),
+      rgba(255, 255, 255, 0)
+    );
+    transform: rotate(25deg);
+    animation: ${effectBg} 2s infinite;
+  }
 `;
