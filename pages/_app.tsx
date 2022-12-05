8aff283acdeb15066ee730292dd9312f42ad16e2
fe.hyunsu <fe.hyunsu@gmail.com>
2022-12-05 23:45:17 +0900

    modify : React.StrictMode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
@@ -10,30 +11,32 @@ import Header from "../components/layout/header";
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <RecoilRoot>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
-          />
-          <meta property="og:title" content="AccountBook" />
-          <meta
-            property="og:description"
-            content="React study group accountBook"
-          />
-          <meta property="og:image" content="/meta.png" />
-          <link rel="icon" href="/favicon.ico" />
-          <title>🥸 AccountBook Admin</title>
-        </Head>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <Header />
-          <main>
-            <Component {...pageProps} />
-          </main>
-          <Footer />
-        </ThemeProvider>
-      </RecoilRoot>
+      <React.StrictMode>
+        <RecoilRoot>
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
+            />
+            <meta property="og:title" content="AccountBook" />
+            <meta
+              property="og:description"
+              content="React study group accountBook"
+            />
+            <meta property="og:image" content="/meta.png" />
+            <link rel="icon" href="/favicon.ico" />
+            <title>🥸 AccountBook Admin</title>
+          </Head>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>
+            <Header />
+            <main>
+              <Component {...pageProps} />
+            </main>
+            <Footer />
+          </ThemeProvider>
+        </RecoilRoot>
+      </React.StrictMode>
     </>
   );
 };
